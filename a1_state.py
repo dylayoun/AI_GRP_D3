910ee37e4d694cc29865d8cb0f768e96fdb71615
dylayoun <16dylayoun@gmail.com>
2025-10-06 16:30:55 +0100

    file create

---
+++ a1_state.py
@@ -0,0 +1,14 @@
+"""
+Hinger Project
+Coursework 001 for: CMP-6058A Artificial Intelligence
+
+Includes a State class for Task 1
+
+@author: Dylan Young (100889423)
+@date:   29/09/2025
+
+"""
+class State:
+    {
+        
+    }
---
+++ header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Hinger Project
+Coursework 001 for: CMP-6058A Artificial Intelligence
+
+Includes a State class for Task 1
+
+@author: A9 (12345, 5678, and 89123)
+@date:   29/09/2025
+
+"""
